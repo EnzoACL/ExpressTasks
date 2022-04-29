4934258392a532d0baa0b83d28bda32aa3a44336
Enzo <enzo.camilolado@gmail.com>
2022-04-29 13:44:48 +0200

    Database creada(aun no funciona)

---
+++ src/dataBase.mjs
@@ -0,0 +1,56 @@
+import sqlite3 from "sqlite3";
+//solo se crea la tabla de users
+
+const db = new sqlite3.Database('src/tasks.db', (err)=>{
+    if (err) {
+        console.log(err.message);
+    }
+    console.log('Conected to the tasks application database')
+});
+
+db.run (`
+    CREATE TABLE
+        IF NOT EXISTS
+        users(
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+`);
+
+db.run(`
+    CREATE TABLE
+        IF NOT EXISTS
+        tasks (
+            id INTEGER PRIMARY KEY,
+            description TEXT NOT NULL
+            done INTEGER
+        )
+`);
+
+export function sqlCallback (error, data) {
+    console.log("error:",error, "data:", data);
+    if (error) throw error;
+}
+
+export function insertUser ( userObject, callback ) {
+    const { id, name, password } = userObject;
+    const sql = `
+        INSERT INTO users (id, name, password)
+        values (${id}, "${name}", "${password}");
+    `;
+    db.run(sql,callback);
+}
+
+export function insertTask (taskObject, callback) {
+    const { id, description, done } = taskObject;
+    const sql = `
+    INSERT INTO tasks (id, description, done)
+    values (${id}, "${description}, ${done})
+    `;
+    db.run(sql,callback)
+}
+
+
+
+export default db;
